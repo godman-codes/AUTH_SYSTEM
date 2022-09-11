27ea5e099a6432b45b682c98f8ce06ed826aa92b
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-09-11 11:33:40 +0100

    creating a custom user model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+class UserAccountManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError('User must have an email address')
+
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save()
+
+        return user 
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True) # this is to overwrite the default django behavior of using username as login parameters
+    name = models.CharField(max_length=255)
+    is_active =  models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email' # Email for unique authentication instead of username 
+    REQUIRED_FIELDS = ['name'] # this are fields that must be specified before the request is made
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self) -> str:
+        return f'Account {self.email}'
