90ca37bb3bbec854c41e1542d638eb8ca37b7f98
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-09-15 17:04:46 +0100

    implemented the login page using redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,25 +6,35 @@ import Signup from "./containers/Signup";
 import ResetPassword from "./containers/ResetPassword";
 import ResetPasswordConfirm from "./containers/ResetPasswordConfirm";
 import Activate from "./containers/Activate";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
 import Layout from "./hocs/Layout";
 
 const App = () => (
-   <Router>
-      <Layout>
-         <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/signup" element={<Signup />} />
-            <Route exact path="/reset-password" element={<ResetPassword />} />
-            <Route
-               exact
-               path="/password/reset/confirm/:uid/:token"
-               element={<ResetPasswordConfirm />}
-            />
-            <Route exact path="/activate" element={<Activate />} />
-         </Routes>
-      </Layout>
-   </Router>
+   <Provider store={store}>
+      <Router>
+         <Layout>
+            <Routes>
+               <Route exact path="/" element={<Home />} />
+               <Route exact path="/login" element={<Login />} />
+               <Route exact path="/signup" element={<Signup />} />
+               <Route
+                  exact
+                  path="/reset-password"
+                  element={<ResetPassword />}
+               />
+               <Route
+                  exact
+                  path="/password/reset/confirm/:uid/:token"
+                  element={<ResetPasswordConfirm />}
+               />
+               <Route exact path="/activate" element={<Activate />} />
+            </Routes>
+         </Layout>
+      </Router>
+   </Provider>
 );
 
 export default App;
--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { login } from "../actions/auth";
 
-const Login = () => {
-   return <div>Login</div>;
+const Login = ({ login }) => {
+   const [formData, setFormData] = useState({
+      email: "",
+      password: "",
+   });
+
+   const { email, password } = formData;
+
+   const onChange = (e) =>
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+
+   const onSubmit = (e) => {
+      e.preventDefault();
+      login(email, password);
+   };
+
+   // is the user authenticated?
+   // redirect them to the homepage
+   return (
+      <div className="container mt-5">
+         <h1>Sign In</h1>
+         <p>Sign into your Account</p>
+         <form onSubmit={(e) => onSubmit(e)}>
+            <div className="form-group">
+               <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={email}
+                  className="form-control"
+                  onChange={(e) => onChange(e)}
+                  required
+               />
+               <br />
+               <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={password}
+                  className="form-control"
+                  onChange={(e) => onChange(e)}
+                  required
+                  minLength="6"
+               />
+            </div>
+            <br />
+            <button type="submit" className="btn btn-primary">
+               Login
+            </button>
+         </form>
+         <p className="mt-3">
+            Don't have an account? <Link to="/signup">Signup</Link>
+         </p>
+         <p className="mt-3">
+            Forgot Your Password?{" "}
+            <Link to="/reset-password">Reset Password</Link>
+         </p>
+      </div>
+   );
 };
+// const mapStateToProps = (state) => ({
+//    //isAuthenticated:
+// });
 
-export default Login;
+export default connect(null, { login })(Login);
