1d69e9057494eb699e8da90cf5e1d554b8b7331e
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-09-16 14:41:39 +0100

    implemeted the navbar , added sum beauti to the home page and also made the navbar link responsive to when the user is authenticated or not

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -3,8 +3,48 @@ import {
    LOGIN_FAIL,
    USER_LOADED_SUCCESS,
    USER_LOADED_FAIL,
+   AUTHENTICATED_FAIL,
+   AUTHENTICATED_SUCCESS,
+   LOGOUT,
 } from "./types";
 import axios from "axios";
+
+export const checkAuthenticated = () => async (dispatch) => {
+   if (localStorage.getItem("access")) {
+      const config = {
+         headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+         },
+      };
+      const body = JSON.stringify({ token: localStorage.getItem("access") });
+      try {
+         const res = await axios.post(
+            `${process.env.REACT_APP_API_URL}/auth/jwt/verify/`,
+            body,
+            config
+         );
+         console.log(res.data);
+         if (res.data.code !== "token_not_valid") {
+            dispatch({
+               type: AUTHENTICATED_SUCCESS,
+            });
+         } else {
+            dispatch({
+               type: AUTHENTICATED_FAIL,
+            });
+         }
+      } catch (err) {
+         dispatch({
+            type: AUTHENTICATED_FAIL,
+         });
+      }
+   } else {
+      dispatch({
+         type: AUTHENTICATED_FAIL,
+      });
+   }
+};
 
 export const load_user = () => async (dispatch) => {
    if (localStorage.getItem("access")) {
@@ -20,6 +60,7 @@ export const load_user = () => async (dispatch) => {
             `${process.env.REACT_APP_API_URL}/auth/users/me/`,
             config
          );
+         console.log(res.data);
          dispatch({
             type: USER_LOADED_SUCCESS,
             payload: res.data,
@@ -58,4 +99,9 @@ export const login = (email, password) => async (dispatch) => {
          type: LOGIN_FAIL,
       });
    }
+};
+export const logout = () => (dispatch) => {
+   dispatch({
+      type: LOGOUT,
+   });
 };
