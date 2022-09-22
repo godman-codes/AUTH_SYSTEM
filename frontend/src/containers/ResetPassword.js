3c3b94206e4a951e43b3cfe69aad780b68831aaf
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-09-22 13:16:05 +0100

    working on the reset password aspect of the authentication

--- frontend/src/containers/ResetPassword.js
+++ frontend/src/containers/ResetPassword.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
+import { connect } from "react-redux";
+import { reset_password } from "../actions/auth";
 
-const ResetPassword = () => {
-   return <div>ResetPassword</div>;
+const ResetPassword = ({ reset_password }) => {
+   const [requestSent, setRequestSent] = useState(false);
+   const [formData, setFormData] = useState({
+      email: "",
+   });
+
+   const { email } = formData;
+
+   const onChange = (e) =>
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+
+   const onSubmit = (e) => {
+      e.preventDefault();
+      reset_password(email);
+      setRequestSent(true);
+   };
+
+   if (requestSent) {
+      return <Navigate to="/" />;
+   }
+   return (
+      <div className="container mt-5">
+         <h1>Reset Password</h1>
+         <form onSubmit={(e) => onSubmit(e)}>
+            <div className="form-group">
+               <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={email}
+                  className="form-control"
+                  onChange={(e) => onChange(e)}
+                  required
+               />
+            </div>
+            <br />
+            <button type="submit" className="btn btn-primary">
+               Reset Password
+            </button>
+         </form>
+      </div>
+   );
 };
 
-export default ResetPassword;
+export default connect(null, { reset_password })(ResetPassword);
--- frontend/src/containers/ResetPasswordConfirm.js
+++ frontend/src/containers/ResetPasswordConfirm.js
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import { Navigate, useParams } from "react-router-dom";
+import { connect } from "react-redux";
+import { reset_password_confirm } from "../actions/auth";
 
-const ResetPasswordConfirm = () => {
-   return <div>ResetPasswordConfirm</div>;
+const ResetPasswordConfirm = ({ match, reset_password_confirm }) => {
+   const [requestSent, setRequestSent] = useState(false);
+   const [formData, setFormData] = useState({
+      new_password: "",
+      re_new_password: "",
+   });
+   const { uid, token } = useParams();
+   const { new_password, re_new_password } = formData;
+
+   const onChange = (e) =>
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+
+   const onSubmit = (e) => {
+      e.preventDefault();
+      reset_password_confirm(uid, token, new_password, re_new_password);
+      setRequestSent(true);
+   };
+
+   if (requestSent) {
+      return <Navigate to="/login" />;
+   }
+   return (
+      <div className="container mt-5">
+         <h1>Reset Password</h1>
+         <form onSubmit={(e) => onSubmit(e)}>
+            <div className="form-group">
+               <input
+                  type="password"
+                  name="new_password"
+                  placeholder="New Password"
+                  value={new_password}
+                  className="form-control"
+                  onChange={(e) => onChange(e)}
+                  required
+               />
+               <br />
+               <input
+                  type="Password"
+                  name="re_new_password"
+                  placeholder="Confirm New Password"
+                  value={re_new_password}
+                  className="form-control"
+                  onChange={(e) => onChange(e)}
+                  required
+               />
+            </div>
+            <br />
+            <button type="submit" className="btn btn-primary">
+               Reset Password
+            </button>
+         </form>
+      </div>
+   );
 };
 
-export default ResetPasswordConfirm;
+export default connect(null, { reset_password_confirm })(ResetPasswordConfirm);
